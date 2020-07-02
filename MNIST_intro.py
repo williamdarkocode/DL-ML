88e5fd23c86d41323df31533e6fbe1cc6361fb88
William <williamd8323@afsenyc.org>
2020-07-02 17:16:38 -0400

    intro to keras

---
+++ MNIST_intro.py
@@ -0,0 +1,28 @@
+import keras
+from keras.datasets import mnist
+from keras import models
+from keras import layers
+from keras.utils import to_categorial
+
+(train_images, train_labels), (tes_images, test_labels) = mnist.load_data()
+
+
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+network.add(layers.Dense(10, activation='softmax'))
+
+
+network.compile(optimizer='rmsprop', loss='categorial_crossentropy', metrics=['accuracy'])
+
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32')/255
+
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = tes_images.astype('float32')/255
+
+
+train_labels = to_categorial(train_labels)
+test_labels = to_categorial(test_labels)
+
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
