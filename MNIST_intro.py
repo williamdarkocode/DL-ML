5d24504baec93ac310ba60b7ccea5aef2853c090
William <williamd8323@afsenyc.org>
2020-07-23 12:45:36 -0400

    correct implementation of mnist example

--- MNIST_intro.py
+++ MNIST_intro.py
@@ -3,27 +3,24 @@ from tensorflow import keras
 from keras.datasets import mnist
 from keras import models
 from keras import layers
-from keras.utils import to_categorial
+from keras.utils import to_categorical
 
-(train_images, train_labels), (tes_images, test_labels) = mnist.load_data()
 
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 network.add(layers.Dense(10, activation='softmax'))
 
-
-network.compile(optimizer='rmsprop', loss='categorial_crossentropy', metrics=['accuracy'])
-
+network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32')/255
 
 test_images = test_images.reshape((10000, 28 * 28))
-test_images = tes_images.astype('float32')/255
+test_images = test_images.astype('float32')/255
 
-
-train_labels = to_categorial(train_labels)
-test_labels = to_categorial(test_labels)
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
 
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
