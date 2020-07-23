fd5ca0ea71548d736743b92a0b1e4c3f2a4d3061
William <williamd8323@afsenyc.org>
2020-07-23 14:22:16 -0400

    meging

--- MNIST_intro.py
+++ MNIST_intro.py
@@ -3,7 +3,7 @@ from tensorflow import keras
 from keras.datasets import mnist
 from keras import models
 from keras import layers
-from keras.utils import to_categorial
+from keras.utils import to_categorical
 
 (train_images, train_labels), (tes_images, test_labels) = mnist.load_data()
 
@@ -23,7 +23,7 @@ test_images = test_images.reshape((10000, 28 * 28))
 test_images = tes_images.astype('float32')/255
 
 
-train_labels = to_categorial(train_labels)
-test_labels = to_categorial(test_labels)
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
 
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
