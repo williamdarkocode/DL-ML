e678da2e9960671b539136fec67f189a74cba34d
William <williamd8323@afsenyc.org>
2020-07-28 20:06:40 -0400

    imbd example with issues

---
+++ BinaryClassification_IMDB_Example.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import imdb
+from keras import models
+from keras import layers
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+word_index = imdb.get_word_index()
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+# print(reverse_word_index)
+
+decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+
+def vectorise_sequences(sequences, dimension=1000):
+    results = np.zeros((len(sequences), 'object', dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorise_sequences(train_data)
+x_test = vectorise_sequences(test_data)
+
+y_train = np.array(train_labels, dtype=object).astype('float32')
+y_test = np.array(test_labels, dtype=object).astype('float32')
+
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics= ['accuracy']) 
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=[x_val, y_val])
+
+history_dict = history.history
+
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+acc = history_dict['acc']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
