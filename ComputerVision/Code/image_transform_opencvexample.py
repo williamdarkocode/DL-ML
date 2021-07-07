91bcd87602903a3899c49689684db333cf769b5c
William <williamd8323@afsenyc.org>
2021-07-07 14:41:35 -0400

    image transformations opencv basics

--- ComputerVision/Code/image_transform_opencvexample.py
+++ ComputerVision/Code/image_transform_opencvexample.py
@@ -7,8 +7,8 @@ import imutils
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the input image")
 
-ap.add_argument("-t", "--transformation", required=True, nargs='+', choices=('translate', 'rotate', 'resize'), 
-help="One of: 'translate', 'rotate', 'resize'")
+ap.add_argument("-t", "--transformation", required=True, nargs='+', choices=('translate', 'rotate', 'resize', 'flip', 'crop'), 
+help="One of: 'translate', 'rotate', 'resize', 'flip', 'crop' ")
 
 ap.add_argument("-x", "--shift_x", required=False, nargs=1, 
 help="The number of pixels to shift on the x axis. Negative values for left, positive for right.")
@@ -21,6 +21,11 @@ ap.add_argument("-c", "--centre", required=False, nargs=1, help="Centre of rotat
 
 ap.add_argument("-hr", "--height_ratio", required=False, nargs=1, help="Ratio of new height to old height")
 ap.add_argument("-wr", "--width_ratio", required=False, nargs=1, help="Ratio of new width to old width")
+
+ap.add_argument("-fd", "--flip_direction", required=False, nargs=1, choices=('0','1','-1'), help="Integer for direction to flip")
+
+ap.add_argument("-ca", "--crop_area", required=False, nargs=1, choices=('cr', 'tl', 'tr', 'bl', 'br'), 
+help="One of the following: cr, tl, tr, bl, br, for centre, top-left, top-right, bottom-left, bottom-right, respectively.")
 
 args = vars(ap.parse_args())
 
@@ -57,13 +62,26 @@ def switch(t):
         final_image = imutils.resize(final_image, width=nw, height=nh)
         # cv2.imshow("resized {} px height, {} px width".format(nh, nw), final_image)
         text += "resized {} px height, {} px width ".format(nh, nw)
-    
-
+    if t == "flip":
+        d = int(args["flip_direction"][0])
+        sfd = "horizontally" if d == 1 else "vertically"
+        sfd = "both horizontally, and vertically" if d == -1 else sfd
+        final_image = cv2.flip(final_image, d)
+        text+= "flipped {}".format(sfd)
+    if t == "crop":
+        crop_area = args["crop_area"][0]
+        final_image = image[0:int(image.shape[0]/2),0:int(image.shape[1]/2)] if crop_area == "tl" else final_image
+        final_image = image[:-int(image.shape[0]/2),-int(image.shape[1]/2):] if crop_area == "tr" else final_image
+        final_image = image[-int(image.shape[0]/2):,:-int(image.shape[1]/2)] if crop_area == "bl" else final_image
+        final_image = image[-int(image.shape[0]/2):,-int(image.shape[1]/2):] if crop_area == "br" else final_image
+        final_image = image[int(image.shape[0]/4):-int(image.shape[0]/4),int(image.shape[1]/4):-int(image.shape[1]/4)] if crop_area == "cr" else final_image
+        text+= "cropped area: {}".format(crop_area)
 
 def transform():
+    # print("Image shape: {} ".format(image.shape))
     for t in transformation:
         switch(t)
-    cv2.putText(final_image, text, (10,20), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.4, color=(0,0,255), thickness=1)
+    cv2.putText(final_image, text, (10,20), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.4, color=(255,0,0), thickness=1)
     cv2.imshow("Transformed image", final_image)
     cv2.waitKey(0)
 
